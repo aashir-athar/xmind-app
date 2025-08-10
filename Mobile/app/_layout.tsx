de0e728815a944e3a69a21086df05608aa146f07
aashir-athar <iamaashirathar@gmail.com>
2025-08-10 21:29:34 +0500

    tabs setup and signout added

--- Mobile/app/_layout.tsx
+++ Mobile/app/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import "../global.css";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
+      <StatusBar style="dark" />
     </ClerkProvider>
   );
 }
